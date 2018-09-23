b4dd512dd6c153cba61e965bd1c999f5002ca5b2
ThexCrack10 <thexcrack10@hotmail.com>
2018-09-23 16:54:14 -0300

    Modelo Base
    Modelo funcionando

--- Tesis/backend/admin.py
+++ Tesis/backend/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from backend.models import *
 
 # Register your models here.
+
+admin.site.register(RubroComercio)
+admin.site.register(Categoria)
+admin.site.register(Caracteristica)
+admin.site.register(UnidadDeMedida)
+admin.site.register(TipoCodigo)
+admin.site.register(CaracteristicaProducto)
+admin.site.register(CodigoProducto)
+admin.site.register(MedidaProducto)
+admin.site.register(Comercio)
+admin.site.register(Producto)
--- Tesis/backend/models.py
+++ Tesis/backend/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class RubroComercio (models.Model): 
+    pass
+
+class Categoria (models.Model): 
+    pass
+
+class Caracteristica (models.Model): 
+    pass
+
+class UnidadDeMedida (models.Model): 
+    pass
+
+class TipoCodigo (models.Model): 
+    pass
+
+class CaracteristicaProducto (models.Model): 
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    caracteristica = models.ForeignKey(Caracteristica, on_delete=models.CASCADE)
+    valor = models.TextField()
+
+class CodigoProducto (models.Model):    
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    codigo = models.ForeignKey(TipoCodigo, on_delete=models.CASCADE)
+    valor = models.TextField()
+
+class MedidaProducto (models.Model): 
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    unidad = models.ForeignKey(UnidadDeMedida, on_delete=models.CASCADE)
+    valor = models.FloatField()
+
+class Comercio (models.Model): 
+    rubro = models.ForeignKey(RubroComercio, on_delete=models.CASCADE)
+
+class Producto (models.Model): 
+    categorias = models.ManyToManyField(Categoria)
+    comercios = models.ManyToManyField(Comercio)
+    caracteristicas = models.ManyToManyField(Caracteristica, through='CaracteristicaProducto')
+    codigos = models.ManyToManyField(TipoCodigo, through='CodigoProducto')
+    medidas = models.ManyToManyField(UnidadDeMedida, through='MedidaProducto')
+
+
+
